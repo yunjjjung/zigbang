81a7b0816a3f4f94e7bd04fec2c7fa8b5841c2a2
최윤정 <jjjung1003@naver.com>
2021-02-28 12:25:56 +0900

    adapter 뼈대 작업 (2) - getView 내부 초안 완성

--- app/src/main/java/com/thejoeun/zigbang/adapters/RoomAdapter.kt
+++ app/src/main/java/com/thejoeun/zigbang/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.thejoeun.zigbang.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.thejoeun.zigbang.R
 import com.thejoeun.zigbang.datas.Room
 
 class RoomAdapter(
@@ -10,5 +14,20 @@ class RoomAdapter(
     val mList : ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
 
 
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
 
 }
