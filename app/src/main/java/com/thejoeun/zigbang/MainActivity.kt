b91e458c300dcd051665146aba61e6c56c5187f1
최윤정 <jjjung1003@naver.com>
2021-02-28 12:40:50 +0900

    어댑터 객체 생성 => 리스트뷰와 연결

--- app/src/main/java/com/thejoeun/zigbang/MainActivity.kt
+++ app/src/main/java/com/thejoeun/zigbang/MainActivity.kt
@@ -2,17 +2,21 @@ package com.thejoeun.zigbang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.thejoeun.zigbang.adapters.RoomAdapter
 import com.thejoeun.zigbang.datas.Room
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRoomList = ArrayList<Room>()
 
+    lateinit var mAdapter: RoomAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         mRoomList.add(Room(8000, "서울시 종로구", 4, "종로의 4층 방"))
         mRoomList.add(Room(8500, "고양시 종로구", -3, "종로의 4층 방"))
@@ -26,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         mRoomList.add(Room(9200, "서울시 관악구", 4, "종로의 4층 방"))
         mRoomList.add(Room(15000, "서울시 강서구", 0, "종로의 4층 방"))
         mRoomList.add(Room(20000, "서울시 은평구", 9, "종로의 4층 방"))
+
+        mAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mAdapter
 
     }
 }
