70163286e5bc0e967baf18d93ea3c521ea4d0de0
최윤정 <jjjung1003@naver.com>
2021-02-28 12:36:53 +0900

    실제 방 데이터 추가 => 설명 문구만 미비한 상태

--- app/src/main/java/com/thejoeun/zigbang/MainActivity.kt
+++ app/src/main/java/com/thejoeun/zigbang/MainActivity.kt
@@ -6,11 +6,26 @@ import com.thejoeun.zigbang.datas.Room
 
 class MainActivity : AppCompatActivity() {
 
-    val mBangList = ArrayList<Room>()
+    val mRoomList = ArrayList<Room>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mRoomList.add(Room(8000, "서울시 종로구", 4, "종로의 4층 방"))
+        mRoomList.add(Room(8500, "고양시 종로구", -3, "종로의 4층 방"))
+        mRoomList.add(Room(7000, "안양시 종로구", 6, "종로의 4층 방"))
+        mRoomList.add(Room(7500, "천안시 종로구", 0, "종로의 4층 방"))
+        mRoomList.add(Room(18000, "안산시 종로구", 5, "종로의 4층 방"))
+        mRoomList.add(Room(12000, "서울시 구로구", 7, "종로의 4층 방"))
+        mRoomList.add(Room(10000, "서울시 영등포구", 8, "종로의 4층 방"))
+        mRoomList.add(Room(16000, "서울시 동작구", 5, "종로의 4층 방"))
+        mRoomList.add(Room(9500, "서울시 강남구", -2, "종로의 4층 방"))
+        mRoomList.add(Room(9200, "서울시 관악구", 4, "종로의 4층 방"))
+        mRoomList.add(Room(15000, "서울시 강서구", 0, "종로의 4층 방"))
+        mRoomList.add(Room(20000, "서울시 은평구", 9, "종로의 4층 방"))
+
     }
 }
